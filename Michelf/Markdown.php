e2678506101da654e37bcb970e01bc1160155c33
Chunliang Lu <aptenom@gmail.com>
2013-04-14 02:59:58 -0700

    add three backticks as block code.

--- Michelf/Markdown.php
+++ Michelf/Markdown.php
@@ -2477,7 +2477,7 @@ class _MarkdownExtra_TmpImpl extends \Michelf\Markdown {
 				(?:\n|\A)
 				# 1: Opening marker
 				(
-					~{3,} # Marker: three tilde or more.
+					[~`]{3,} # Marker: three tilde or more.
 				)
 				[ ]*
 				(?:
